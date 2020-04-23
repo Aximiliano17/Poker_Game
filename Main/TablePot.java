8eb04fdc829b967e3da1f1b51715174f9f01088f
Fernando Samano <SamanoF21@gmail.com>
2020-04-22 23:20:34 -0700

    Optimized
    Decided to combine both my SidePot and TablePot classes.

--- Main/TablePot.java
+++ Main/TablePot.java
@@ -1,25 +1,61 @@
 /**
- * This class adds 
+ * This class keeps track of the mainPot, as well as each player sidePot. If placer C goes all in with 300, and player D
+ * raises to 700, even if player C wins, he can't win more than 300 from each player.This feature is not yet implemented
+ * but will be soon.
  */
 package Main;
 
+import java.util.HashMap;
+
 public class TablePot {
-private int tablePot;
-public TablePot()
+private int mainPot;
+private int maxBid;//the newest amount that a player has raised to.
+private HashMap<Player,Integer> currentBets=new HashMap<>();//Keeps track of players with their currentBet
+
+public void addToMainPot(int n)
 {
+	mainPot+=n;
 }
-public void increaseTablePot(int n)
+public int getMainPot()
 {
-	tablePot+=n;
+	return mainPot;
 }
-public int getTablePot()
+public int distributeMainPot()//Method to give the whole amount to the winner/s.
 {
-	return tablePot;
+	int total=mainPot;
+	mainPot=mainPot-mainPot;
+	return total;
 }
-public int giveTablePot()
+public int getCurrentBet(Player player)
 {
-	int total=tablePot;
-	tablePot=tablePot-tablePot;
+	if(!currentBets.containsKey(player))
+	{
+		currentBets.put(player,0);
+	}
+	return currentBets.get(player);
+}
+public void setCurrentBet(Player player,int amount)
+{
+	amount+=getCurrentBet(player);
+	currentBets.put(player,amount);
+}
+public int getMaxBid()
+{
+	return maxBid;
+}
+public void increaseBid(int a)//When a player raises, increases the amount of maxBid
+{
+	maxBid+=a;
+}
+//Calculates the total bet amount from all players and returns it.
+public int getBetsTotal()
+{
+	int total=0;
+	for(Player p:currentBets.keySet())
+	{
+		total+=currentBets.get(p);
+		currentBets.put(p,0);
+	}
 	return total;
 }
 }
