231ea75a3ecea1fa74c46c3b18af99be1b3a9d72
Fernando Samano <SamanoF21@gmail.com>
2020-04-22 23:23:38 -0700

    Optimized
    Removed some methods, decided to make players static instead of having to pass a reference to game or a reference to players.

--- Main/PokerGame.java
+++ Main/PokerGame.java
@@ -10,14 +10,12 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 
 public class PokerGame extends JFrame {
-	private List<Player> players = new ArrayList<Player>(6);
-	private List<Player> allowedPlayers = new CircularArrayList<Player>(6);// only players with money >10 are allowed to play
-	private int turnTracker;
+	private static List<Player> players = new ArrayList<Player>(6);
 
 	public static void main(String[] args) {
 		PokerGame game = new PokerGame();
-		game.readData();
-		game.add(new MenuPane(game));
+		readData();
+		game.add(new MenuPane());
 		game.setSize(400, 400);
 		game.setLocationRelativeTo(null);
 		game.setTitle("Poker Game");
@@ -25,7 +23,7 @@ public class PokerGame extends JFrame {
 		game.setVisible(true);
 	}
 
-	public void readData() {
+	public static void readData() {
 		String filename = ("data.txt");
 		Scanner inputStream = null;
 		try {
@@ -43,7 +41,7 @@ public class PokerGame extends JFrame {
 		System.out.println("Saved Data from file has been loaded");
 	}
 
-	public void saveData(List<Player> players) {
+	public static void saveData(List<Player> players) {
 		setPlayers(players);
 		String filename = ("data.txt");
 		PrintWriter outputStream = null;
@@ -62,31 +60,11 @@ public class PokerGame extends JFrame {
 		outputStream.close();
 	}
 
-	public List<Player> getPlayers() {
+	public static List<Player> getPlayers() {
 		return players;
 	}
 
-	public void setPlayers(List<Player> players) {
-		this.players = players;
-	}
-
-	public List<Player> getAllowedPlayers() {
-		return allowedPlayers;
-	}
-
-	public void setAllowedPlayers(List<Player> allowedPlayers) {
-		this.allowedPlayers = allowedPlayers;
-	}
-
-	public int getTurn() {
-		return this.turnTracker;
-	}
-
-	public void increaseTurn() {
-		this.turnTracker++;
-	}
-
-	public void resetTracker() {
-		this.turnTracker = 0;
+	public static void setPlayers(List<Player> p) {
+		players = p;
 	}
 }
